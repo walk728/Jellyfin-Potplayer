c4cbd129adcb0484c1ac0f7f64111a5e1ab9ebcc
walk728 <walk728@qq.com>
2022-02-18 22:10:18 +0800

    Update potplayer.ps1
    修改在smb共享下使用

--- potplayer.ps1
+++ potplayer.ps1
@@ -2,5 +2,6 @@ Add-Type -Assembly System.Web
 $path=$args[0]
 $path=$path -replace "potplayer://" , ""
 $path= [System.Web.HttpUtility]::UrlDecode($path)
+$path=$path -replace "/" , "\"
 echo $path
 & "C:\\Program Files\\DAUM\\PotPlayer\\PotPlayerMini64.exe" $path
